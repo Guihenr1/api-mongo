7d1e3cfea8b561cc980adc5a176c24174cca529d
Guilherme <guilherme.pomp@gmail.com>
2021-03-01 20:45:09 -0300

    Projeto concluido

--- Api.Mongo/Startup.cs
+++ Api.Mongo/Startup.cs
@@ -21,20 +21,15 @@ namespace Api.Mongo {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddSingleton<Data.MongoDB>();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
